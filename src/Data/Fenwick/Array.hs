471d926440afff66afd68b1273e5ba7b7e6a3ad0
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2025-03-20 13:39:25 +0330

    Remove Data.Group in favor of Cancellative

--- src/Data/Fenwick/Array.hs
+++ src/Data/Fenwick/Array.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE BangPatterns #-}
 
-module Data.Fenwick.Array where
+module Data.Fenwick.Array 
+  ( FenMArray
+  , newFen
+  , addFen
+  , sumPrefixFen
+  , sumRangeFen
+  ) where
 
 import Data.Array.MArray
 import Control.Monad
 import Data.Bits
-import Data.Group
+import Data.Semigroup.Cancellative
+import Data.Monoid.Cancellative
+import Data.Maybe
 
 data FenMArray array elem = FenMArray Int (array Int elem)
 
@@ -15,30 +23,34 @@ modifyArray' arr i f = do
   let !x' = f x
   writeArray arr i x'
 
-newFen :: (MArray array elem m, Monoid elem) => Int -> m (FenMArray array elem)
+newFen :: (MArray array elem m, CommutativeMonoid elem) => Int -> m (FenMArray array elem)
 newFen n = do
   arr <- newArray (1, n) mempty
   pure (FenMArray n arr)
+{-# INLINABLE newFen #-}
 
-addFen :: (MArray array elem f, Semigroup elem) => FenMArray array elem -> Int -> elem -> f ()
+addFen :: (MArray array elem f, Commutative elem) => FenMArray array elem -> Int -> elem -> f ()
 addFen (FenMArray n arr) r a = do
   -- 0 <= r < n - 1
   let go i = when (i <= n) $ do
         modifyArray' arr i (a <>)
         go (i + (i .&. (-i)))
   go (r + 1)
+{-# INLINABLE addFen #-}
 
-sumPrefixFen :: (MArray array elem f, Monoid elem) => FenMArray array elem -> Int -> f elem
+sumPrefixFen :: (MArray array elem f, CommutativeMonoid elem) => FenMArray array elem -> Int -> f elem
 sumPrefixFen (FenMArray n arr) r = do
   let go !s i = if i <= 0 then pure s else do
         x <- readArray arr i
         go (x <> s) (i - (i .&. (-i)))
   go mempty r
+{-# INLINABLE sumPrefixFen #-}
 
-sumRangeFen :: (MArray array elem m, Group elem) => FenMArray array elem -> Int -> Int -> m elem
+sumRangeFen :: (MArray array elem m, CancellativeMonoid elem) => FenMArray array elem -> Int -> Int -> m elem
 sumRangeFen _ l r
   | l >= r = pure mempty
 sumRangeFen f l r = do
   ra <- sumPrefixFen f r
   la <- sumPrefixFen f l
-  pure (ra <> inverse la)
+  pure . fromJust $ ra </> la
+{-# INLINEABLE sumRangeFen #-}
--- src/Data/Modulo.hs
+++ src/Data/Modulo.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE RoleAnnotations #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
 
 module Data.Modulo (
   Modulo(..),
@@ -12,57 +13,60 @@ module Data.Modulo (
 
 import GHC.TypeLits
 import Data.Proxy
+import Foreign.Storable
+import Data.Semigroup.Cancellative
+import Numeric.Product.Commutative
 
 type role Modulo nominal nominal
-newtype Modulo a (m :: Nat) = UnsafeModulo a
-  deriving (Eq, Ord)
+newtype Modulo a (m :: Nat) = Modulo a
+  deriving (Eq, Ord, Storable)
 
 getMod :: (Num a, KnownNat m) => Proxy m -> a
 getMod = fromInteger . natVal
 
 instance (KnownNat m, Show a) => Show (Modulo a m) where
-  show (UnsafeModulo a) = "(" <> show a <> " mod " <> show m' <> ")" where
+  show (Modulo a) = "(" <> show a <> " mod " <> show m' <> ")" where
     m' = getMod (Proxy @m)
 
 instance (KnownNat m, Integral a) => Num (Modulo a m) where
-  (UnsafeModulo !a) + (UnsafeModulo !b)
-    | a + b >= m' = UnsafeModulo (a + b - m')
-    | otherwise = UnsafeModulo (a + b) where 
+  (Modulo !a) + (Modulo !b)
+    | a + b >= m' = Modulo (a + b - m')
+    | otherwise = Modulo (a + b) where 
         m' = getMod (Proxy @m)
 
-  (UnsafeModulo !a) * (UnsafeModulo !b) = UnsafeModulo $ (a * b) `mod` m' where
+  (Modulo !a) * (Modulo !b) = Modulo $ (a * b) `mod` m' where
     m' = getMod (Proxy @m)
 
   abs = id
 
   signum = const 1
 
-  fromInteger n = UnsafeModulo . fromInteger $ n `mod` m' where
+  fromInteger n = Modulo . fromInteger $ n `mod` m' where
     m' = getMod (Proxy @m)
 
-  negate (UnsafeModulo !n) = UnsafeModulo $ if n == 0 then 0 else m' - n where
+  negate (Modulo !n) = Modulo $ if n == 0 then 0 else m' - n where
     m' = getMod (Proxy @m)
 
 instance (KnownNat m, Integral a) => Real (Modulo a m) where
-  toRational (UnsafeModulo a) = toRational a
+  toRational (Modulo a) = toRational a
 
 instance (KnownNat m, Num a) => Bounded (Modulo a m) where
-  minBound = UnsafeModulo 0
-  maxBound = UnsafeModulo (m' - 1) where
+  minBound = Modulo 0
+  maxBound = Modulo (m' - 1) where
     m' = getMod (Proxy @m)
 
 instance (KnownNat m, Integral a) => Enum (Modulo a m) where
-  succ (UnsafeModulo a)
+  succ (Modulo a)
     | a >= m' - 1 = error "succ (mod - 1)"
-    | otherwise = UnsafeModulo (a + 1) where
+    | otherwise = Modulo (a + 1) where
         m' = getMod (Proxy @m)
-  pred (UnsafeModulo 0) = error "pred 0"
-  pred (UnsafeModulo a) = UnsafeModulo (a - 1)
+  pred (Modulo 0) = error "pred 0"
+  pred (Modulo a) = Modulo (a - 1)
   toEnum a
     | a >= m' = error "toEnum larger than modulo"
-    | otherwise = UnsafeModulo (fromIntegral a) where
+    | otherwise = Modulo (fromIntegral a) where
         m' = getMod (Proxy @m)
-  fromEnum (UnsafeModulo a) = fromEnum a
+  fromEnum (Modulo a) = fromEnum a
 
 binPow :: Num a => a -> Int -> a
 binPow _ 0 = 1
@@ -73,8 +77,11 @@ binPow !a b
     !x = binPow (a * a) (b `div` 2)
 
 instance (KnownNat m, Integral a) => Integral (Modulo a m) where
-  quotRem _ (UnsafeModulo 0) = error "divide by zero"
+  quotRem _ (Modulo 0) = error "divide by zero"
   quotRem a b = (a * (b `binPow` (m' - 2)), 0) where
     m' = getMod (Proxy @m)
   divMod = quotRem
-  toInteger (UnsafeModulo a) = toInteger a
+  toInteger (Modulo a) = toInteger a
+
+instance (KnownNat m, Integral a) => SumCancellative (Modulo a m)
+instance (KnownNat m, Integral a) => CommutativeProduct (Modulo a m)
